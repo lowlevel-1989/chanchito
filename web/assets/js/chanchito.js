a7f16a398f9a0457fe1329bf66328fad1fff5e4c
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-01-09 12:58:11 -0430

    Se elimino archivo no necesarios

--- web/assets/js/chanchito.js
+++ web/assets/js/chanchito.js
@@ -1,1 +1,21 @@
-var myApp = angular.module('chanchito', ['ngRoute']);
+(function(){
+	angular.module('chanchito', ['ngRoute'])
+	.run(function ($rootScope, $location, $window, AuthenticationService) {
+		AuthenticationService.isAuthenticated = $window.sessionStorage.isAuthenticated;
+		$rootScope.$on("$routeChangeStart", function (event, nextRoute, currentRoute) {
+			try {
+				if (($location.path() === '/' || $location.path() === '/login') 
+				&& AuthenticationService.isAuthenticated){
+						
+					$location.path('/home');
+				}
+
+				if (nextRoute.access.requiredLogin && !AuthenticationService.isAuthenticated) {
+					$location.path('/');
+				}
+			}catch(err) {
+				console.log('Redirect: #/');
+			}
+		});
+	});
+})();
--- web/assets/js/urls.js
+++ web/assets/js/urls.js
@@ -26,22 +26,3 @@ myApp.config(function ($routeProvider) {
     	redirectTo: '/working'
     });
 });
-
-myApp.run(function ($rootScope, $location, $window, AuthenticationService) {
-    AuthenticationService.isAuthenticated = $window.sessionStorage.isAuthenticated;
-    $rootScope.$on("$routeChangeStart", function (event, nextRoute, currentRoute) {
-    try {
-        if (($location.path() === '/' || $location.path() === '/login') && AuthenticationService.isAuthenticated){
-            $location.path('/home');
-        }
-
-        if (nextRoute.access.requiredLogin && !AuthenticationService.isAuthenticated) {
-            $location.path('/');
-        }
-    }
-    catch(err) {
-        console.log('Redirect: #/');
-    }
-
-    });
-});
