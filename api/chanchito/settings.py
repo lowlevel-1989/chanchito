a5bc999d749267157edadaf8ec299b76fff90391
Vinicio Valbuena <formatcomvinicio@gmail.com>
2015-01-15 14:22:31 -0430

    Se configuraron los archivos estaticos y se complemento los modelos de base de datos

--- api/chanchito/settings.py
+++ api/chanchito/settings.py
@@ -61,11 +61,18 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+
+# Esto configura la STATIC_URL cuando esta en distribucion
+
+import os
+_PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
+_API = '/' + _PROJECT_DIR.split('/')[-2] + '/api/'
+
+STATIC_URL = _API + STATIC_ROOT 
 
 # Additional locations of static files
 STATICFILES_DIRS = (
--- api/movements/models.py
+++ api/movements/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Active(models.Model):
 	date        = models.DateField(auto_now=True)
-	description = models.TextField()
+	description = models.TextField(blank=True)
 	active      = models.BooleanField(default=True)
 
 class Movement(models.Model):
@@ -16,7 +16,10 @@ class Movement(models.Model):
 
 	def save(self, *args, **kwargs):
 		if not self.pk:
-			active = Active(description='no se a modificado')
+			active = Active()
 			active.save()
 			self.active = active
 		super(Movement, self).save(*args, **kwargs)
+
+	def __unicode__(self):
+		return self.description
